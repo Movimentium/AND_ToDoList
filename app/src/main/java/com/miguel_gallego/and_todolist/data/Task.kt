3038681d33ef3864d4087114b0892fe1bbe59d87
Miguel Gallego <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-15 13:54:44 +0200

    TaskListActivity.kt added

--- app/src/main/java/com/miguel_gallego/and_todolist/data/Task.kt
+++ app/src/main/java/com/miguel_gallego/and_todolist/data/Task.kt
@@ -10,12 +10,16 @@ class Task(
         const val COLUMN_ID = "id"
         const val COLUMN_TITLE = "title"
         const val COLUMN_DONE = "done"
+        const val COLUMN_CATEGORY = "category_id"
 
         const val SQL_CREATE_TABLE =
             "CREATE TABLE $TABLE_NAME (" +
                     "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     "$COLUMN_TITLE TEXT, " +
-                    "$COLUMN_DONE BOOLEAN)"
+                    "$COLUMN_DONE BOOLEAN, " +
+                    "$COLUMN_CATEGORY INTEGER, " +
+                    "FOREIGN KEY($COLUMN_CATEGORY) REFERENCES ${Category.TABLE_NAME}(${Category.COLUMN_ID}) " +
+                    "ON DELETE CASCADE)"  //TODO: THINK the behavior  // ON DELETE RESTRICT // ON DELETE SET NULL
 
         const val SQL_DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
     }
