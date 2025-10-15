54733df6d55a6e2eaa6d12141e98e18bd5803cde
Miguel Gallego <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-15 10:29:11 +0200

    CategoryDAO.kt getCategoryWithId(id:) done

--- app/src/main/java/com/miguel_gallego/and_todolist/data/CategoryDAO.kt
+++ app/src/main/java/com/miguel_gallego/and_todolist/data/CategoryDAO.kt
@@ -39,6 +39,7 @@ class CategoryDAO(
         val values = ContentValues()
         values.put(Category.COLUMN_NAME, category.name)
         try {
+            open()
             val updatedRows = db.update(Category.TABLE_NAME, values, "${Category.COLUMN_ID} = ${category.id}", null)
             L.log("Updated: $updatedRows in table ${Category.TABLE_NAME}")
         } catch (e: Exception) {
@@ -47,4 +48,86 @@ class CategoryDAO(
             close()
         }
     }
+
+    fun delete(id: Int) {
+        try {
+            open()
+            val deteledRows = db.delete(Category.TABLE_NAME, "${Category.COLUMN_ID} = $id", null)
+            L.log("Deleted: $deteledRows in table ${Category.TABLE_NAME}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            close()
+        }
+    }
+
+    fun getCategoryWithId(id: Int): Category? {
+        var category: Category? = null
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query
+        var projection = arrayOf(Category.COLUMN_ID, Category.COLUMN_NAME)
+
+        // Filter results WHERE "id" = 'myId'
+        val selection = "${Category.COLUMN_ID} = $id"
+        val selectionArgs = null
+        val sortOrder = null
+        try {
+            val cursor = db.query(
+                Category.TABLE_NAME,       // table name
+                projection,             // array of cols to return (write null to get all)
+                selection,                        // cols for the WHERE clause
+                selectionArgs,                    // vals for the WHERE clause
+                null,                   // don't group the rows
+                null,                    // don't filter by row groups
+                sortOrder,
+            )
+            // Read the cursor data
+            if (cursor.moveToNext()) {
+                val id = cursor.getInt(cursor.getColumnIndexOrThrow(Category.COLUMN_ID))
+                val name = cursor.getString(cursor.getColumnIndexOrThrow(Category.COLUMN_NAME))
+                category = Category(id, name)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            close()
+        }
+        return  category
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
