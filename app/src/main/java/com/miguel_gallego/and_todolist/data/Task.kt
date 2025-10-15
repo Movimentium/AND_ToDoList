4b413a45cffe8091fa649e8916125413b3f3476e
Miguel Gallego <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-15 10:52:52 +0200

    Task.kt done

---
+++ app/src/main/java/com/miguel_gallego/and_todolist/data/Task.kt
@@ -0,0 +1,22 @@
+package com.miguel_gallego.and_todolist.data
+
+class Task(
+    val id: Int,
+    val title: String,
+    val done: Boolean
+) {
+    companion object {
+        const val TABLE_NAME = "Task"
+        const val COLUMN_ID = "id"
+        const val COLUMN_TITLE = "title"
+        const val COLUMN_DONE = "done"
+
+        const val SQL_CREATE_TABLE =
+            "CREATE TABLE $TABLE_NAME (" +
+                    "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "$COLUMN_TITLE TEXT, " +
+                    "$COLUMN_DONE BOOLEAN)"
+
+        const val SQL_DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
+    }
+}
