5dcf71608867b6ad0735f7f57f471a303d88c56c
Miguel Gallego <miguel.0.1.1.2.3.5.8@gmail.com>
2025-10-15 13:04:46 +0200

    CategoryActivity.kt done

--- app/src/main/java/com/miguel_gallego/and_todolist/activities/CategoryActivity.kt
+++ app/src/main/java/com/miguel_gallego/and_todolist/activities/CategoryActivity.kt
@@ -1,21 +1,44 @@
 package com.miguel_gallego.and_todolist.activities
 
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.miguel_gallego.and_todolist.R
+import com.miguel_gallego.and_todolist.data.Category
+import com.miguel_gallego.and_todolist.data.CategoryDAO
+import com.miguel_gallego.and_todolist.databinding.ActivityCategoryBinding
 
 class CategoryActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityCategoryBinding
+    lateinit var categoryDAO: CategoryDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_category)
+
+        categoryDAO = CategoryDAO(this)
+
+        binding = ActivityCategoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnSave.setOnClickListener {
+            val categoryName = binding.txtEditName.editText?.text.toString()
+            if (categoryName != null) {
+                val category = Category(-1, categoryName)
+                categoryDAO.insert(category)
+                finish()  // CategoryActivity disappears
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+
 }
